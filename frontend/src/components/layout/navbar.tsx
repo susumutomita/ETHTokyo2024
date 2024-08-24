95c2f039dc21d3a763714d11cfac598b63875ce9
susumu tomita <oyster880@gmail.com>
2024-08-24 21:39:27 +0900

    add logo

--- frontend/src/components/layout/navbar.tsx
+++ frontend/src/components/layout/navbar.tsx
@@ -16,21 +16,20 @@ export default function NavBar({ session }: { session: Session | null }) {
     <>
       <SignInModal />
       <div
-        className={`fixed top-0 w-full flex justify-center ${
-          scrolled
-            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
-            : "bg-white/0"
-        } z-30 transition-all`}
+        className={`fixed top-0 w-full flex justify-center ${scrolled
+          ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl"
+          : "bg-white/0"
+          } z-30 transition-all`}
       >
         <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
           <Link href="/" className="flex items-center font-display text-2xl">
             <Image
-              src="/logo.png"
+              src="/logo.png"  // ここを変更しました
               alt="ChefConnect logo"
-              width="30"
-              height="30"
+              width={30}
+              height={30}
               className="mr-2 rounded-sm"
-            ></Image>
+            />
             <p>ChefConnect</p>
           </Link>
           <div>
