52212cb9422515f19fd004c33832b797e4ca2daa
susumu tomita <oyster880@gmail.com>
2024-08-25 04:19:32 +0900

    add smartcontract

--- frontend/src/app/constants/contract.tsx
+++ frontend/src/app/constants/contract.tsx
@@ -2,121 +2,226 @@ export const contractAddress = "0x70d2356cB6a642cd296F6774D08a118D7ccf8DBb";
 
 export const abi = [
   {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [{ internalType: "address", name: "owner", type: "address" }],
+    name: "OwnableInvalidOwner",
+    type: "error",
+  },
+  {
+    inputs: [{ internalType: "address", name: "account", type: "address" }],
+    name: "OwnableUnauthorizedAccount",
+    type: "error",
+  },
+  {
+    anonymous: false,
     inputs: [
+      { indexed: true, internalType: "address", name: "chef", type: "address" },
+      { indexed: false, internalType: "string", name: "name", type: "string" },
+      { indexed: false, internalType: "string", name: "description", type: "string" },
+      { indexed: false, internalType: "string", name: "specialty", type: "string" },
+    ],
+    name: "ChefProfileSubmitted",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "previousOwner", type: "address" },
+      { indexed: true, internalType: "address", name: "newOwner", type: "address" },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "uint256", name: "serviceId", type: "uint256" },
+      { indexed: true, internalType: "address", name: "provider", type: "address" },
+      { indexed: false, internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "PaymentReleased",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [{ indexed: true, internalType: "uint256", name: "serviceId", type: "uint256" }],
+    name: "ServiceCancelled",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [{ indexed: true, internalType: "uint256", name: "serviceId", type: "uint256" }],
+    name: "ServiceCompleted",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "uint256", name: "serviceId", type: "uint256" },
+      { indexed: true, internalType: "address", name: "provider", type: "address" },
+      { indexed: true, internalType: "address", name: "customer", type: "address" },
+      { indexed: false, internalType: "uint256", name: "amount", type: "uint256" },
+    ],
+    name: "ServiceCreated",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [{ indexed: true, internalType: "uint256", name: "serviceId", type: "uint256" }],
+    name: "ServiceStarted",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "chef", type: "address" },
+      { indexed: false, internalType: "uint256", name: "voteCount", type: "uint256" },
+    ],
+    name: "Voted",
+    type: "event",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "serviceId", type: "uint256" }],
+    name: "cancelService",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "chefAddresses",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "chefProfiles",
+    outputs: [
+      { internalType: "string", name: "name", type: "string" },
+      { internalType: "string", name: "description", type: "string" },
+      { internalType: "string", name: "specialty", type: "string" },
+      { internalType: "uint256", name: "voteCount", type: "uint256" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "serviceId", type: "uint256" }],
+    name: "completeService",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address payable", name: "_provider", type: "address" }],
+    name: "createService",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getChefProfiles",
+    outputs: [
+      { internalType: "address[]", name: "", type: "address[]" },
       {
-        internalType: "string",
-        name: "content",
-        type: "string",
+        components: [
+          { internalType: "string", name: "name", type: "string" },
+          { internalType: "string", name: "description", type: "string" },
+          { internalType: "string", name: "specialty", type: "string" },
+          { internalType: "uint256", name: "voteCount", type: "uint256" },
+        ],
+        internalType: "struct CateringEscrow.ChefProfile[]",
+        name: "",
+        type: "tuple[]",
       },
     ],
-    name: "submitSenryu",
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+    ],
+    name: "hasVoted",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "serviceId", type: "uint256" }],
+    name: "releasePayment",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "services",
+    outputs: [
+      { internalType: "uint256", name: "id", type: "uint256" },
+      { internalType: "address payable", name: "provider", type: "address" },
+      { internalType: "address payable", name: "customer", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" },
+      { internalType: "enum CateringEscrow.ServiceStatus", name: "status", type: "uint8" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "serviceId", type: "uint256" }],
+    name: "startService",
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "uint256",
-        name: "senryuId",
-        type: "uint256",
-      },
+      { internalType: "string", name: "_name", type: "string" },
+      { internalType: "string", name: "_description", type: "string" },
+      { internalType: "string", name: "_specialty", type: "string" },
     ],
+    name: "submitChefProfile",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "newOwner", type: "address" }],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "_chef", type: "address" }],
     name: "vote",
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "uint",
-        name: "page",
-        type: "uint",
-      },
-      {
-        internalType: "uint",
-        name: "pageSize",
-        type: "uint",
-      },
-    ],
-    name: "getSenryus",
-    outputs: [
-      {
-        components: [
-          {
-            internalType: "uint256",
-            name: "id",
-            type: "uint256",
-          },
-          {
-            internalType: "string",
-            name: "content",
-            type: "string",
-          },
-          {
-            internalType: "address",
-            name: "author",
-            type: "address",
-          },
-          {
-            internalType: "uint256",
-            name: "voteCount",
-            type: "uint256",
-          },
-        ],
-        internalType: "struct SenryuGame.Senryu[]",
-        name: "",
-        type: "tuple[]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint",
-        name: "page",
-        type: "uint",
-      },
-      {
-        internalType: "uint",
-        name: "pageSize",
-        type: "uint",
-      },
-    ],
-    name: "getTopSenryus",
-    outputs: [
-      {
-        components: [
-          {
-            internalType: "uint256",
-            name: "id",
-            type: "uint256",
-          },
-          {
-            internalType: "string",
-            name: "content",
-            type: "string",
-          },
-          {
-            internalType: "address",
-            name: "author",
-            type: "address",
-          },
-          {
-            internalType: "uint256",
-            name: "voteCount",
-            type: "uint256",
-          },
-        ],
-        internalType: "struct SenryuGame.Senryu[]",
-        name: "",
-        type: "tuple[]",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
+    stateMutability: "payable",
+    type: "receive",
   },
 ];
